566931484eeff9dd95b3a4fbacea3bb3ac592174
Lucie Trinquart <lucietrinquart26@gmail.com>
2024-10-04 11:13:14 +0200

    fait le style et placer les éléments dans la page detail

--- src/app/pages/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.component.ts
@@ -21,6 +21,21 @@ export class PokemonDetailComponent {
   pokemon!: Pokemon;
   pokemonevolution!: PokemonChain;
   Moves: Move[] = [];
+  visibleMoves: any[] = [];
+  showAll: boolean = false;
+
+  ngOnInit() {
+    this.visibleMoves = this.Moves.slice(0, 6);
+  }
+
+  toggleShowMore() {
+    if (this.showAll) {
+      this.visibleMoves = this.Moves.slice(0, 6);
+    } else {
+      this.visibleMoves = this.Moves;
+    }
+    this.showAll = !this.showAll;
+  }
 
   getBackgroundClass(): string {
     console.log(this.pokemon?.default_variety);
@@ -58,6 +73,43 @@ export class PokemonDetailComponent {
     }
     return '';
   }
+
+  getBackgroundColor(): string {
+    console.log(this.pokemon?.default_variety);
+    
+    if (this.pokemon?.default_variety?.types && this.pokemon.default_variety.types.length > 0) {
+      const primaryType = this.pokemon.default_variety.types[0].name.toLowerCase();
+      const className = `h-4 bg-${primaryType}-500 rounded-full dark:bg-${primaryType}-500 `;
+      console.log(className);
+      return className;
+    }
+    return '';
+  }
+
+  getBackgroundbordeur(): string {
+    console.log(this.pokemon?.default_variety);
+    
+    if (this.pokemon?.default_variety?.types && this.pokemon.default_variety.types.length > 0) {
+      const primaryType = this.pokemon.default_variety.types[0].name.toLowerCase();
+      const className = `m-2 p-2 border-2 border-${primaryType}-500 rounded-lg shadow-lg neon-effect text-center w-full border-blur-effect `;
+      console.log(className);
+      return className;
+    }
+    return '';
+  }
+
+  getBackgroundbouton(): string {
+    console.log(this.pokemon?.default_variety);
+    
+    if (this.pokemon?.default_variety?.types && this.pokemon.default_variety.types.length > 0) {
+      const primaryType = this.pokemon.default_variety.types[0].name.toLowerCase();
+      const className = `px-4 py-2 bg-${primaryType}-500 text-white rounded-lg hover:bg-${primaryType}-700`;
+      console.log(className);
+      return className;
+    }
+    return '';
+  }
+
   @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
   playCry() {
     this.audioPlayer.nativeElement.play();
@@ -84,6 +136,7 @@ export class PokemonDetailComponent {
             this.apiService.requestApi(`/pokemon/${params['pokemon_id']}/evolution`)
             .then((response: PokemonChain) => {
               this.pokemonevolution = response; // Stocker les données d'évolution dans une propriété distincte
+
             })
 
             
